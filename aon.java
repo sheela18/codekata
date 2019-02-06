b6dd045d8c9296f2e51e2b5f52de29a6c3739679
sheela18 <47381013+sheela18@users.noreply.github.com>
2019-02-06 15:49:39 +0530

    Create aon.java

---
+++ aon.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+  class aon
+  {
+
+    public static void main(String[] args) 
+    {
+
+        char ch = '*';
+        if(ch>='a' && ch<='z' || ch>='A' && ch <= 'Z' )
+        System.out.println(ch +"alphabet");
+        else
+        System.out.println("not alphabet");
+
+       
+    }
+}
