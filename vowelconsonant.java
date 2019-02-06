3bb479dfa54719d281ba2b86cec9e7a75fafedad
sheela18 <47381013+sheela18@users.noreply.github.com>
2019-02-06 15:31:01 +0530

    Create vowelconsonant.java

---
+++ vowelconsonant.java
@@ -0,0 +1,23 @@
+public class vowelconsonant {
+
+    public static void main(String[] args) {
+
+        char ch = 'z';
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " vowel");
+                break;
+            default:
+                if(ch>='a'&&ch<='z')
+                System.out.println(ch + "consonant");
+                else
+                System.out.println(ch + " invalid");
+                }
+        
+    }
+}
