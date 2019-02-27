379dd4041952617bf4fcca06aab8f21b6b9004c9
sheela18 <47381013+sheela18@users.noreply.github.com>
2019-02-27 15:30:21 +0530

    Update largest .java

--- largest .java
+++ largest .java
@@ -6,7 +6,6 @@ public static void main(String[] args)
 {
 int a,b,c,largest;
 Scanner s=new Scanner(System.in);
-System.out.println("Enter any three numbers: ");
 a=s.nextInt();
 b=s.nextInt();
 c=s.nextInt();
