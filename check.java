0f1655576d219cfc164472d8c9d952547cda456f
sheela18 <47381013+sheela18@users.noreply.github.com>
2019-03-05 11:04:34 +0530

    Update check.java

--- check.java
+++ check.java
@@ -1,24 +1,14 @@
-import java.util.Scanner;
+
  class check
 {
     public static void main(String[] args) 
     {
         int number;
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the number you want to check:");
-        number = scan.nextInt();
-        scan.close();
         if(number > 0)
-        {
-            System.out.println(number+" is positive number");
-        }
+            System.out.println("positive ");
         else if(number < 0)
-        {
-            System.out.println(number+" is negative number");
-        }
+            System.out.println( "negative");
         else
-        {
-            System.out.println(number+" is default number");
-        }
+            System.out.println("default");
     }
 }
