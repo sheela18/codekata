6c9357367fb2330c0ff5cac56341e1685641b470
sheela18 <47381013+sheela18@users.noreply.github.com>
2019-03-05 11:45:19 +0530

    Update year.java

--- year.java
+++ year.java
@@ -1,9 +1,17 @@
- Scanner input=new Scanner(System.in);
- int a;
+
+
+class year
+{
+public static void main(String[] args) 
+{
+  Scanner input=new Scanner(System.in);
+  int a;
   System.out.println("enter the year ");
     a=input.nextInt();
     if ((a % 4 ==0 ) && (a%100!=0) || (a%400==0)) 
         System.out.println("leep year");
     else 
         System.out.println("not a leap year");
-      
+}
+}
+
