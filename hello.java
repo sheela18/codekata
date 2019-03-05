bc9bfdfe4a67abce242c07609db0db8354b9f490
sheela18 <47381013+sheela18@users.noreply.github.com>
2019-03-05 12:19:47 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+ class Hello
+{
+	public static void main(String[] args)
+	{
+	     Scanner sj=new Scanner(System.in);
+	    int n=sj.nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.println("Hello ");
+		}
+	}
+ 
+}
