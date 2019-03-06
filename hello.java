5ab3d53ab0545ac6a96cf6451581fd2304936cb7
sheela18 <47381013+sheela18@users.noreply.github.com>
2019-03-06 15:40:09 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,8 +2,8 @@
 {
 	public static void main(String[] args)
 	{
-	     Scanner sj=new Scanner(System.in);
-	    int n=sj.nextInt();
+	     Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
 		for (int i = 0; i < n; i++) {
 			System.out.println("Hello ");
 		}
