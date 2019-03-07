17a7722626837f8c16075f1e2c0b4d3e72dfcaad
sheela18 <47381013+sheela18@users.noreply.github.com>
2019-03-07 16:17:40 +0530

    Update count.java

--- count.java
+++ count.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
  class count {
-   public static void main(String args[]){
+   public static void main(String args[])
+   {
       Scanner sc = new Scanner(System.in);
       int count =0;
       int num = sc.nextInt();
-      while(num!=0){
+      while(num!=0)
+      {
          num = num/10;
          count++;
       }
