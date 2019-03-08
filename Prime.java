93a0cf3abf8288c3007bbee999f07ec6298a4448
sheela18 <47381013+sheela18@users.noreply.github.com>
2019-03-08 12:07:51 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,8 +1,6 @@
  class Prime {
-
-    public static void main(String[] args) {
-
-        int num = 33, i = 2;
+public static void main(String[] args) {
+int num = 33, i = 2;
         boolean flag = false;
         while(i <= num/2)
         {
