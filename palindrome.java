ea9fdbd50b4f3ed007c9e86527a82f40af4a446d
sheela18 <47381013+sheela18@users.noreply.github.com>
2019-03-08 11:49:06 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,18 @@
+    import java.util.*;   
+   public class palindrome
+    {  
+       public static void main(String args[])  
+       {  
+          String original, reverse = ""; 
+          Scanner in = new Scanner(System.in);   
+          System.out.println("enter no palindrome/not");  
+          original = in.nextLine();   
+          int length = original.length();   
+          for ( int i = length - 1; i >= 0; i-- )  
+             reverse = reverse + original.charAt(i);  
+          if (original.equals(reverse))  
+             System.out.println("yes ");  
+          else  
+             System.out.println("no");   
+       }  
+    }  
