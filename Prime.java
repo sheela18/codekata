01ab89402e86dee100534bb98c275b3e60aab30e
sheela18 <47381013+sheela18@users.noreply.github.com>
2019-03-08 12:03:09 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,22 @@
+ class Prime {
+
+    public static void main(String[] args) {
+
+        int num = 33, i = 2;
+        boolean flag = false;
+        while(i <= num/2)
+        {
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+         ++i;
+        }
+
+        if (!flag)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
